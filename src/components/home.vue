c53ed419daad5a934ac623c1c6791f79cde4cc7e
赵伟杰 <wei-jie.zhao@utsoft.cn>
2017-10-17 12:20:32 +0800

    更改样式

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,6 +1,16 @@
 <template>
     <div>
         <p>基础模板</p>
+        <Button>Default</Button>
+        <Button type="primary">Primary</Button>
+        <Button type="ghost">Ghost</Button>
+        <Button type="dashed">Dashed</Button>
+        <Button type="text">Text</Button>
+        <br><br>
+        <Button type="info">信息按钮</Button>
+        <Button type="success">成功按钮</Button>
+        <Button type="warning">警告按钮</Button>
+        <Button type="error">错误按钮</Button>
     </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -5,8 +5,7 @@ import store from "./vuex/store";
 import router from "./router";
 import axios from "./api";
 import "./styles/reset.css";
-import "iview/dist/styles/iview.css";
-import "./theme/index.less";
+import 'iview/dist/styles/iview.css';
 
 Vue.config.productionTip = false;
 Vue.use(iView);
