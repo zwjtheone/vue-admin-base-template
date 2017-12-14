169f467faef7797e3202201f1b2258a0aa440bc3
赵伟杰 <wei-jie.zhao@reeed.org>
2017-12-14 17:25:23 +0800

    加入图片压缩，关闭打包map

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
          * Source Maps
          */
         
-        productionSourceMap: true,
+        productionSourceMap: false,
         // https://webpack.js.org/configuration/devtool/#production
         devtool: "#source-map",
         // Gzip off by default as many popular static hosts such as
